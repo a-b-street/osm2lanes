ec6d7eecfee0e60481fbd423630b30e66260e6c6
droogmic <droogmic@users.noreply.github.com>
2022-01-24 09:26:26 +0000

    deploy: df175c43782dfb2498e35e777bc0b5075d022475

--- index-88a163ab75b49891.js
+++ index-88a163ab75b49891.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-d8602c39167dd78e_bg.wasm', import.meta.url);
+        input = new URL('index-88a163ab75b49891_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -606,7 +606,7 @@ async function init(input) {
     imports.wbg.__wbg_removeAttribute_1adaecf6b4d35a09 = function() { return handleError(function (arg0, arg1, arg2) {
         getObject(arg0).removeAttribute(getStringFromWasm0(arg1, arg2));
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper3682 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3684 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 49, __wbg_adapter_18);
         return addHeapObject(ret);
     };
