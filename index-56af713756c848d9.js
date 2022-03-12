40a1b793adfccb2f2345fd70c7dfc8b617bd6d9b
dabreegster <dabreegster@users.noreply.github.com>
2022-03-12 09:17:41 +0000

    deploy: 7fa5993615648238e06cfe8f3f74a57b8e9307ed

--- index-56af713756c848d9.js
+++ index-56af713756c848d9.js
@@ -298,7 +298,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-abdcf0bdc9960dbc_bg.wasm', import.meta.url);
+        input = new URL('index-56af713756c848d9_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
