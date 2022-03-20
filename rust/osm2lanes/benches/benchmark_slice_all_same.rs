521d1b5e45c730ebeeb30b5615e8fc1b81483205
Michael Droogleever Fortuyn <droogmic@gmail.com>
2022-03-20 20:49:00 +0100

    Fix Missing

---
+++ rust/osm2lanes/benches/benchmark_slice_all_same.rs
@@ -0,0 +1,52 @@
+use std::hash::Hash;
+
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+
+fn all_same_vec<T: Clone + PartialEq>(v: &[T]) -> bool {
+    if v.is_empty() {
+        return false;
+    }
+    let v: Vec<T> = v.to_vec();
+    v.windows(2).all(|w| w[0] == w[1])
+}
+
+fn all_same_hashmap<T>(v: &[T]) -> bool
+where
+    T: Clone + PartialEq + Eq + Hash,
+{
+    if v.is_empty() {
+        return false;
+    }
+    let v = std::collections::HashSet::<&T>::from_iter(v.iter());
+    v.len() == 1
+}
+
+fn bench_all_same(c: &mut Criterion) {
+    let mut group = c.benchmark_group("slice_all_same");
+    let small_vec = vec![20; 5];
+    group.bench_with_input(
+        BenchmarkId::new("all_same_vec", "small_vec"),
+        &small_vec,
+        |b, small_vec| b.iter(|| all_same_vec(black_box(small_vec))),
+    );
+    group.bench_with_input(
+        BenchmarkId::new("all_same_hashmap", "small_vec"),
+        &small_vec,
+        |b, small_vec| b.iter(|| all_same_hashmap(black_box(small_vec))),
+    );
+    let big_vec = vec![20; 500];
+    group.bench_with_input(
+        BenchmarkId::new("all_same_vec", "big_vec"),
+        &big_vec,
+        |b, big_vec| b.iter(|| all_same_vec(black_box(big_vec))),
+    );
+    group.bench_with_input(
+        BenchmarkId::new("all_same_hashmap", "big_vec"),
+        &big_vec,
+        |b, big_vec| b.iter(|| all_same_hashmap(black_box(big_vec))),
+    );
+    group.finish();
+}
+
+criterion_group!(benches, bench_all_same);
+criterion_main!(benches);
