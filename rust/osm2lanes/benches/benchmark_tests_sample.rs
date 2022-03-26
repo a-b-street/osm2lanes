4b6ee4738b28b6bf87a6dfa808832a4d53ecde72
droogmic <droogmic@gmail.com>
2022-03-26 16:08:43 +0100

    Rust: Refactor Bechmarking
    Switch to tool that handles pipes.
    Sample benchmarks to restrict testing time.
    Split into different job.

--- rust/osm2lanes/benches/benchmark_tests_sample.rs
+++ rust/osm2lanes/benches/benchmark_tests_sample.rs
@@ -2,10 +2,16 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use osm2lanes::test::get_tests;
 use osm2lanes::{tags_to_lanes, Locale, TagsToLanesConfig};
 
+const SAMPLE_RATE: u64 = 4;
 pub fn benchmark_tests(c: &mut Criterion) {
     let tests = get_tests();
     let mut group = c.benchmark_group("tests");
-    for test in &tests {
+    for test in tests.iter().filter(|t| {
+        let mut hasher = std::collections::hash_map::DefaultHasher::new();
+        std::hash::Hasher::write(&mut hasher, t.to_string().as_bytes());
+        let hash = std::hash::Hasher::finish(&hasher);
+        hash % SAMPLE_RATE == 0
+    }) {
         let locale = Locale::builder()
             .driving_side(test.driving_side)
             .iso_3166_option(test.iso_3166_2.as_deref())
@@ -14,8 +20,6 @@ pub fn benchmark_tests(c: &mut Criterion) {
             !test.test_ignore_warnings(),
             test.test_include_separators() && test.expected_has_separators(),
         );
-        group.measurement_time(std::time::Duration::from_millis(1000));
-        group.warm_up_time(std::time::Duration::from_millis(500));
         group.bench_with_input(BenchmarkId::from_parameter(test), test, |b, test| {
             b.iter(|| {
                 assert!(tags_to_lanes(&test.tags, &locale, &config).is_ok());
