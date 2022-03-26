fe479f4a887aadd1289ae17297da8c9a30ed87e6
Ben Ritter <benjaminaritter@gmail.com>
2022-03-26 18:40:38 +0800

    Remove unused `#![feature(is_some_with)]`

--- rust/osm2lanes/src/lib.rs
+++ rust/osm2lanes/src/lib.rs
@@ -4,8 +4,6 @@
 //! WARNING: The output specification and all of this code is just being prototyped. Don't depend
 //! on anything yet.
 
-#![feature(is_some_with)]
-
 mod metric;
 pub use metric::{Metre, Speed};
 
