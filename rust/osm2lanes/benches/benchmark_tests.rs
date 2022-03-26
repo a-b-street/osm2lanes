950547d9b103b2ba2203b6d9a414b85733f8d7a7
Michael Droogleever Fortuyn <droogmic@gmail.com>
2022-03-26 13:46:23 +0100

    WIP

--- rust/osm2lanes/benches/benchmark_tests.rs
+++ rust/osm2lanes/benches/benchmark_tests.rs
@@ -6,8 +6,8 @@ use osm2lanes::{tags_to_lanes, Locale, TagsToLanesConfig};
 
 pub fn benchmark_tests(c: &mut Criterion) {
     let tests = get_tests();
-    let mut group = c.benchmark_group("benchmark_tests");
-    for test in tests.iter() {
+    let mut group = c.benchmark_group("tests");
+    for test in tests.iter().step_by(tests.len() / 10) {
         let locale = Locale::builder()
             .driving_side(test.driving_side)
             .iso_3166_option(test.iso_3166_2.as_deref())
@@ -16,8 +16,8 @@ pub fn benchmark_tests(c: &mut Criterion) {
             !test.test_ignore_warnings(),
             test.test_include_separators() && test.expected_has_separators(),
         );
-        group.measurement_time(Duration::from_millis(1000));
-        group.warm_up_time(Duration::from_millis(500));
+        // group.measurement_time(Duration::from_millis(1000));
+        // group.warm_up_time(Duration::from_millis(500));
         group.bench_with_input(BenchmarkId::from_parameter(test), test, |b, test| {
             b.iter(|| {
                 assert!(tags_to_lanes(&test.tags, &locale, &config).is_ok());
