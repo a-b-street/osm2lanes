2425247d6864c60880de184750ac52574aa6d798
Michael Droogleever Fortuyn <droogmic@gmail.com>
2022-04-09 20:33:48 +0200

    Add Leaflet-based Slippy Map To Website
    Working example,
    cleanups will be needed as a follow-up.

---
+++ rust/osm2lanes-web/src/map.rs
@@ -0,0 +1,122 @@
+use gloo_utils::document;
+use leaflet::{LatLng, Map, MouseEvent, Path, Polyline, TileLayer};
+use osm2lanes::locale::Locale;
+use osm2lanes::overpass::{get_nearby, LatLon};
+use osm2lanes::tag::Tags;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{Element, HtmlElement, Node};
+use yew::prelude::*;
+use yew::Html;
+
+#[allow(clippy::large_enum_variant)]
+pub enum Msg {
+    MapClick(LatLng),
+    MapUpdate(Tags, Locale, Vec<LatLon>),
+    Error(String),
+}
+
+pub struct MapComponent {
+    container: HtmlElement,
+    map: Map,
+    lat: Point,
+    _map_click_closure: Closure<dyn Fn(MouseEvent)>,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Point(pub f64, pub f64);
+
+#[derive(Properties, Clone, PartialEq)]
+pub struct Props {
+    pub tags_locale: Callback<(Tags, Locale)>,
+}
+
+impl MapComponent {
+    fn render_map(&self) -> Html {
+        let node: &Node = &self.container.clone().into();
+        Html::VRef(node.clone())
+    }
+}
+
+impl Component for MapComponent {
+    type Message = Msg;
+    type Properties = Props;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let container: Element = document().create_element("div").unwrap();
+        let container: HtmlElement = container.dyn_into().unwrap();
+        container.set_id("map");
+        let map = Map::new_with_element(&container, &JsValue::NULL);
+
+        let map_click_callback = ctx.link().callback(Msg::MapClick);
+        let map_click_closure = Closure::<dyn Fn(MouseEvent)>::wrap(Box::new(move |click_event| {
+            log::debug!("map click");
+            map_click_callback.emit(click_event.latlng());
+        }));
+        map.on("click", map_click_closure.as_ref());
+
+        Self {
+            container,
+            map,
+            lat: Point(40.0, 10.0),
+            // to avoid dropping the closure and invalidating the callback
+            _map_click_closure: map_click_closure,
+        }
+    }
+
+    fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
+        if first_render {
+            self.map.setView(&LatLng::new(self.lat.0, self.lat.1), 2.0);
+            log::debug!("add tile layer");
+            add_tile_layer(&self.map);
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::MapClick(lat_lng) => {
+                ctx.link().send_future(async move {
+                    match get_nearby((lat_lng.lat(), lat_lng.lng())).await {
+                        Ok((tags, locale, geometry)) => Msg::MapUpdate(tags, locale, geometry),
+                        Err(e) => Msg::Error(e.to_string()),
+                    }
+                });
+                true
+            },
+            Msg::MapUpdate(tags, locale, geometry) => {
+                let polyline = Polyline::new(
+                    geometry
+                        .into_iter()
+                        .map(|lat_lon| LatLng::new(lat_lon.lat, lat_lon.lon).into())
+                        .collect(),
+                );
+                let path = Path::from(polyline);
+                path.addTo(&self.map);
+                // self.map.fitBounds(polyline.getBounds());
+                ctx.props().tags_locale.emit((tags, locale));
+                true
+            },
+            Msg::Error(_) => todo!(),
+        }
+    }
+
+    fn changed(&mut self, _ctx: &Context<Self>) -> bool {
+        false
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div class="map-container component-container">
+                {self.render_map()}
+            </div>
+        }
+    }
+}
+
+fn add_tile_layer(map: &Map) {
+    TileLayer::new(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        &JsValue::NULL,
+    )
+    .addTo(map);
+}
--- rust/osm2lanes/src/overpass.rs
+++ rust/osm2lanes/src/overpass.rs
@@ -11,10 +11,17 @@ struct OverpassResult {
 }
 
 #[derive(Debug, Clone, Deserialize)]
+pub struct LatLon {
+    pub lat: f64,
+    pub lon: f64,
+}
+
+#[derive(Debug, Clone, Deserialize)]
 struct Element {
     r#type: ElementType,
     id: ElementId,
     tags: Tags,
+    geometry: Option<Vec<LatLon>>,
 }
 
 #[derive(Debug, Clone, Deserialize, PartialEq)]
@@ -67,7 +74,7 @@ pub async fn get_tags(id: ElementId) -> Result<Tags, reqwest::Error> {
 ///
 /// Unexpected data from overpass and/or openstreetmap.
 ///
-pub async fn get_way(id: ElementId) -> Result<(Tags, Locale), reqwest::Error> {
+pub async fn get_way(id: &ElementId) -> Result<(Tags, Locale), reqwest::Error> {
     let resp = reqwest::Client::new()
         .get(format!(
             r#"https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];
@@ -112,7 +119,54 @@ pub async fn get_way(id: ElementId) -> Result<(Tags, Locale), reqwest::Error> {
         elements.pop().unwrap()
     };
     assert_eq!(way_element.r#type, ElementType::Way);
-    assert_eq!(way_element.id, id);
+    assert_eq!(&way_element.id, id);
 
     Ok((way_element.tags, locale))
+}
+
+/// Get Tags and Geometries from Overpass.
+/// Given a longitude and latitude, find a way within 10m.
+/// If there is more than one way within 10m, returns one at random...
+///
+/// # Errors
+///
+/// May occur when processing a request.
+///
+/// # Panics
+///
+/// Unexpected data from overpass and/or openstreetmap.
+///
+pub async fn get_nearby(
+    lng_lat: (f64, f64),
+) -> Result<(Tags, Locale, Vec<LatLon>), reqwest::Error> {
+    const RADIUS: f64 = 10.0_f64;
+    let mut resp = reqwest::Client::new()
+        .get(format!(
+            r#"https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];
+            way
+            (around:{RADIUS},{},{})
+            ["highway"];
+            out tags geom;"#,
+            lng_lat.0, lng_lat.1
+        ))
+        .send()
+        .await?
+        .json::<OverpassResult>()
+        .await?;
+    log::debug!("{:#?}", resp);
+    if resp.elements.len() > 1 {
+        log::warn!("more than one nearby way found, returning one at random");
+    }
+    resp.elements.truncate(1);
+    let element = resp.elements.pop().unwrap();
+    assert_eq!(element.r#type, ElementType::Way);
+    // TODO: single overpass query
+    let (tags, locale) = get_way(&element.id).await?;
+    Ok((
+        tags,
+        locale,
+        element
+            .geometry
+            .expect("overpass response missing geometry"),
+    ))
 }
