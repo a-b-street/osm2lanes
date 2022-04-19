bef736782e35abf4da12e4128eb4094f58e09921
droogmic <droogmic@users.noreply.github.com>
2022-04-19 15:12:38 +0000

    deploy: a5a40dd79a3370a3bf0592ec466ce53478796e75

--- index-f12fe07df67253d9.js
+++ index-f12fe07df67253d9.js
@@ -302,7 +302,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-4a0c1a25e25e2582_bg.wasm', import.meta.url);
+        input = new URL('index-f12fe07df67253d9_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
