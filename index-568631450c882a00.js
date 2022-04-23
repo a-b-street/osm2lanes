d2ef4cf1028d2c7744bf7fe78700587fb0dfe94d
droogmic <droogmic@users.noreply.github.com>
2022-04-23 12:31:07 +0000

    deploy: c19a8ae74a43863b1fb30db97f3711c9a62ed865

--- index-568631450c882a00.js
+++ index-568631450c882a00.js
@@ -208,7 +208,7 @@ function makeClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_28(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf7e2746b021a9531(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8a5a4358c1aafade(arg0, arg1, addHeapObject(arg2));
 }
 
 function makeMutClosure(arg0, arg1, dtor, f) {
@@ -302,7 +302,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-f12fe07df67253d9_bg.wasm', import.meta.url);
+        input = new URL('index-568631450c882a00_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -818,7 +818,7 @@ async function init(input) {
     imports.wbg.__wbg_removeAttribute_eea03ed128669b8f = function() { return handleError(function (arg0, arg1, arg2) {
         getObject(arg0).removeAttribute(getStringFromWasm0(arg1, arg2));
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper149 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper155 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 24, __wbg_adapter_28);
         return addHeapObject(ret);
     };
