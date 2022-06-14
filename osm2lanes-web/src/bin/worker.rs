f1eff7da19257ab6275b00db2f648080a07b4bfc
Michael Droogleever Fortuyn <droogmic@gmail.com>
2022-06-14 22:34:55 +0200

    Check Upstream

--- osm2lanes-web/src/bin/worker.rs
+++ osm2lanes-web/src/bin/worker.rs
@@ -5,5 +5,5 @@ fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug).expect("logging failed");
     log::trace!("Initializing worker...");
-    ExampleLoader::register();
+    ExampleLoader::registrar().register();
 }
