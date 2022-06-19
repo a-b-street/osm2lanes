5361cc5dfa624093ac774e054cad356bbf425428
droogmic <droogmic@users.noreply.github.com>
2022-06-19 17:14:24 +0000

    deploy: 8ee3ef5fd8b0cf81b2e0b5f910e339d08b485f8c

--- worker.js
+++ worker.js
@@ -137,7 +137,7 @@ function makeClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_16(arg0, arg1, arg2) {
-    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h005a6481bebf7d5d(arg0, arg1, addHeapObject(arg2));
+    wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h91c0a145328e4fed(arg0, arg1, addHeapObject(arg2));
 }
 
 function makeMutClosure(arg0, arg1, dtor, f) {
