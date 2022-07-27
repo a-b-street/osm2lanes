c416c1a751d0f113eafa2fcc1597b698fa502f09
Dustin Carlino <dabreegster@gmail.com>
2022-07-27 12:10:54 +0100

    Wrap editor state into an object

--- web_editor/js/main.js
+++ web_editor/js/main.js
@@ -25,74 +25,95 @@ new Sortable(document.getElementById("delete"), {
   },
 });
 
-// This state is set up by startEditing
-var current_road = null;
-var current_locale = null;
+class LaneEditor {
+  constructor(way, road_wrapper, locale) {
+    const road = road_wrapper["Ok"]["road"];
 
-async function startEditing() {
-  const way = BigInt(document.getElementById("osm_way_id").value);
-  // Faster dev workflow: if the way ID is the default, use baked-in data instead of waiting on Overpass.
-  var road_wrapper, locale;
-  if (way == 804788513) {
-    [road_wrapper, locale] = dummyData();
-  } else {
-    console.log(`Fetching ${way}...`);
-    [road_wrapper, locale] = await js_way_to_lanes(way);
-  }
-  const road = road_wrapper["Ok"]["road"];
-  console.log(`Got osm2lanes output, creating cards`);
+    this.way = way;
+    // Clone these
+    this.current_road = JSON.parse(JSON.stringify(road));
+    this.current_locale = JSON.parse(JSON.stringify(locale));
 
-  current_road = JSON.parse(JSON.stringify(road));
-  current_locale = JSON.parse(JSON.stringify(locale));
+    console.log(`Got osm2lanes output, creating cards`);
 
-  // TODO Fully clean up the old cards, including whatever sortable thing is attached there?
-  const cards = document.getElementById("cards");
-  cards.replaceChildren();
+    // TODO Fully clean up the old cards, including whatever sortable thing is attached there?
+    const cards = document.getElementById("cards");
+    cards.replaceChildren();
 
-  // Create a card per lane
-  for (const lane of road["lanes"]) {
-    if (lane["type"] == "separator") {
-      continue;
-    }
-    cards.appendChild(makeLaneCard(lane));
-  }
-
-  new Sortable(cards, {
-    group: {
-      name: "lanes",
-      put: ["toolbox"],
-    },
-    animation: 150,
-    ghostClass: "card-being-dragged",
-    onAdd: function (evt) {
-      const type = evt.item.getAttribute("value");
-      // TODO switch case but without break?
-      // TODO Figure out direction based on center line position
-      var card;
-      if (type == "driving") {
-        card = makeLaneCard({
-          type: "travel",
-          direction: "backward",
-          designated: "motor_vehicle",
-        });
-      } else if (type == "bicycle") {
-        card = makeLaneCard({
-          type: "travel",
-          direction: "backward",
-          designated: "bicycle",
-        });
-      } else if (type == "parking") {
-        card = makeLaneCard({
-          type: "parking",
-          direction: "backward",
-          designated: "motor_vehicle",
-        });
+    // Create a card per lane
+    for (const lane of road["lanes"]) {
+      if (lane["type"] == "separator") {
+        continue;
       }
-      evt.item.replaceWith(card);
-    },
-  });
+      cards.appendChild(makeLaneCard(lane));
+    }
+
+    new Sortable(cards, {
+      group: {
+        name: "lanes",
+        put: ["toolbox"],
+      },
+      animation: 150,
+      ghostClass: "card-being-dragged",
+      onAdd: function (evt) {
+        const type = evt.item.getAttribute("value");
+        // TODO switch case but without break?
+        // TODO Figure out direction based on center line position
+        var card;
+        if (type == "driving") {
+          card = makeLaneCard({
+            type: "travel",
+            direction: "backward",
+            designated: "motor_vehicle",
+          });
+        } else if (type == "bicycle") {
+          card = makeLaneCard({
+            type: "travel",
+            direction: "backward",
+            designated: "bicycle",
+          });
+        } else if (type == "parking") {
+          card = makeLaneCard({
+            type: "parking",
+            direction: "backward",
+            designated: "motor_vehicle",
+          });
+        }
+        evt.item.replaceWith(card);
+      },
+    });
+  }
+
+  static async create() {
+    var current_road = null;
+    var current_locale = null;
+
+    const way = BigInt(document.getElementById("osm_way_id").value);
+    // Faster dev workflow: if the way ID is the default, use baked-in data instead of waiting on Overpass.
+    var road_wrapper, locale;
+    if (way == 804788513) {
+      [road_wrapper, locale] = dummyData();
+    } else {
+      console.log(`Fetching ${way}...`);
+      [road_wrapper, locale] = await js_way_to_lanes(way);
+    }
+
+    window.editor = new LaneEditor(way, road_wrapper, locale);
+    return window.editor;
+  }
+
+  generateTags() {
+    // Mutate the shared state
+    this.current_road["lanes"] = [];
+    for (const card of document.getElementById("cards").children) {
+      this.current_road["lanes"].push(card.laneJSON);
+    }
+    const tags = js_lanes_to_tags(this.current_road, this.current_locale);
+
+    const output = document.getElementById("output_tags");
+    output.value = tags;
+  }
 }
-window.startEditing = startEditing;
 
 function makeLaneCard(lane) {
   var node = document.createElement("div");
@@ -131,15 +152,7 @@ function makeLaneCard(lane) {
   return node;
 }
 
-function generateTags() {
-  // Mutate the shared state
-  current_road["lanes"] = [];
-  for (const card of document.getElementById("cards").children) {
-    current_road["lanes"].push(card.laneJSON);
-  }
-  const tags = js_lanes_to_tags(current_road, current_locale);
-
-  const output = document.getElementById("output_tags");
-  output.value = tags;
-}
-window.generateTags = generateTags;
+// Global state representing the currently edited way
+window.editor = null;
+// TODO I'm not sure how to make this visible to index.html otherwise
+window.LaneEditor = LaneEditor;
