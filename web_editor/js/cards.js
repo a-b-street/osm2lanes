39f7088e9f3e325fd365bb98816db1a74eede86c
Dustin Carlino <dabreegster@gmail.com>
2022-08-21 14:19:06 +0200

    prettier format

--- web_editor/js/cards.js
+++ web_editor/js/cards.js
@@ -13,51 +13,51 @@ export function makeLaneCard(lane) {
 }
 
 function typeIcon(lane) {
-	if (lane.type == "travel" && lane.designated == "bicycle") {
-		return icon("bicycle");
-	}
-	if (lane.type == "travel" && lane.designated == "bus") {
-		return icon("bus");
-	}
-	if (lane.type == "travel" && lane.designated == "motor_vehicle") {
-		return icon("car");
-	}
-	if (lane.type == "parking" && lane.designated == "motor_vehicle") {
-		return icon("parking");
-	}
+  if (lane.type == "travel" && lane.designated == "bicycle") {
+    return icon("bicycle");
+  }
+  if (lane.type == "travel" && lane.designated == "bus") {
+    return icon("bus");
+  }
+  if (lane.type == "travel" && lane.designated == "motor_vehicle") {
+    return icon("car");
+  }
+  if (lane.type == "parking" && lane.designated == "motor_vehicle") {
+    return icon("parking");
+  }
 
-	return `<b>${lane.type}, ${lane.designated}</b>`;
+  return `<b>${lane.type}, ${lane.designated}</b>`;
 }
 
 function directionIcon(lane) {
-	// TODO Attach an onclick handler. Probably create the below using the DOM instead of innerHTML.
-	if (lane.direction == "forward") {
-		return `<img src="assets/forwards.svg" class="clickable-icon" />`;
-	}
-	if (lane.direction == "backward") {
-		return `<img src="assets/backwards.svg" class="clickable-icon" />`;
-	}
-	if (lane.direction == "both") {
-		return icon("both_ways");
-	}
-	// Just an empty space
-	return "";
+  // TODO Attach an onclick handler. Probably create the below using the DOM instead of innerHTML.
+  if (lane.direction == "forward") {
+    return `<img src="assets/forwards.svg" class="clickable-icon" />`;
+  }
+  if (lane.direction == "backward") {
+    return `<img src="assets/backwards.svg" class="clickable-icon" />`;
+  }
+  if (lane.direction == "both") {
+    return icon("both_ways");
+  }
+  // Just an empty space
+  return "";
 }
 
 function backgroundColor(lane) {
-	if (lane.type == "travel" && lane.designated == "bicycle") {
-		return "#0F7D4B";
-	}
-	return "grey";
+  if (lane.type == "travel" && lane.designated == "bicycle") {
+    return "#0F7D4B";
+  }
+  return "grey";
 }
 
 function width(lane) {
-	if (lane.width) {
-		return `${lane.width}m`;
-	}
-	return "";
+  if (lane.width) {
+    return `${lane.width}m`;
+  }
+  return "";
 }
 
 function icon(name) {
-	return `<img src="assets/${name}.svg" class="icon" />`;
+  return `<img src="assets/${name}.svg" class="icon" />`;
 }
